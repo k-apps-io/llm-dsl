d706d6cbb0a6ab7775419131959bb3157d670cc1
Alan Kopp <alan.kopp@k-apps.io>
2024-03-23 19:16:03 -0500

    Committing changes before beta deploy

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { Chat, CodeBlock, Message } from "./Chat";
 export { toCodeBlock } from "./CodeBlocks";
 export { DSL, Locals, Metadata, Options, Settings } from "./DSL";
 export { ResponseStage, ResponseStageArgs, json } from "./Expect";
-export * as LLM from "./LLM";
+export { Function, FunctionResponse, LLM, Prompt, Stream, TextResponse } from "./LLM";
 export { CODE_BLOCK_RULE, Rule } from "./Rules";
 export { StreamHandler, localFileStorage, localFileStream } from "./Stream";
 export { Visibility, Window, latest, main } from "./Window";
